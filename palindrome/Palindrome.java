f93c79a96b548486243407e65e91d703d9859e79
Jeremy Reynolds <jreynolds323@gmail.com>
2014-08-29 08:49:41 -0500

    finished simple palindrome class.

--- palindrome/Palindrome.java
+++ palindrome/Palindrome.java
@@ -5,8 +5,33 @@
 	A palindrome is a word that is the same any way that you read it.
 	Example: racecar is racecar when you read it backwards or forwards.
 */
+
+import java.util.*;
+
 class Palindrome {
 	public static void main(String[] args) {
 		
+		String original = "";
+		String reversed = "";
+		//created new Scanner object to read user input.
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter a word to be tested as a palindrome.");
+		original = input.next();
+		
+		//get length for original string as condition in for loop
+		int length = original.length();
+		
+		for (int i = length-1; i >= 0; i--) {
+			reversed = reversed + original.charAt(i);
+		}
+		
+		if(original.equals(reversed)){
+			System.out.println("It's a palindrome!");
+		}
+		else {
+			System.out.println("Not a palindrome");
+		}
+				
 	}
 }
