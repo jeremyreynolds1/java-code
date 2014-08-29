76c78f2ed09b78b83a96b322b0dab70ae8c0f4c5
Jeremy Reynolds <jreynolds323@gmail.com>
2014-08-29 07:47:34 -0500

    initial commit of palindrome.java

---
+++ palindrome/Palindrome.java
@@ -0,0 +1,12 @@
+/*
+	Simple palindrome checker based in Java.
+	Created by Jeremy Reynolds on August 29, 2014.
+	
+	A palindrome is a word that is the same any way that you read it.
+	Example: racecar is racecar when you read it backwards or forwards.
+*/
+class Palindrome {
+	public static void main(String[] args) {
+		
+	}
+}
